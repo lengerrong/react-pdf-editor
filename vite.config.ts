e68a5d623a18f86afed304fe3fabf303a8c71f42
Errong Leng <errong.leng@gmail.com>
2023-12-05 23:53:47 -0800

    build as a web app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [
-    dts({ rollupTypes: true }),
     react()
   ],
   esbuild: {
@@ -16,14 +14,6 @@ export default defineConfig({
     },
   },
   build: {
-    target: "esnext",
-    lib: {
-      entry: "src/index.ts",
-      fileName: "index",
-      formats: ["es"]
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime", "react-to-print", "pdfjs-dist", "pdf-lib"],
-    }
+    target: "esnext"
   }
 });
